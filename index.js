2f935cd296700e5bb10acc62330fdf3c65a842b3
abymani <12557637+abymani@users.noreply.github.com>
2022-08-23 15:21:37 +0500

    javascript code improved working

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
 
 
-let subMenu = document.querySelectorAll('.sub-menu');
-let features = document.getElementById('features');
-let company = document.getElementById('company');
-let arrows = document.querySelectorAll('.fa-solid');
-let boldItem = document.querySelectorAll('.bold');
-let navMenus = document.querySelectorAll('a');
+//Handling submenu opening closeing 
+let dropdown= document.querySelectorAll('.dropdown')
 
-boldItem[0].classList.remove('bold');
-boldItem[1].classList.remove('bold');
 
-features.addEventListener ('click',function() {
-    subMenu[0].classList.toggle('active');
-    boldItem[0].classList.toggle('bold');
-    arrows[0].classList.toggle('fa-angle-up');
-});
-
-company.addEventListener ( 'click', function(){
-    subMenu[1].classList.toggle('active');
-    arrows[1].classList.toggle('fa-angle-up');
-    boldItem[1].classList.toggle('bold');
+dropdown.forEach(dropdown => {
+    dropdown.addEventListener('click',function(event){
+        event.currentTarget.classList.toggle('active');
+    })
 })
 
+
 //setting nav menu for mobile view
-let menuIcon = document.getElementById('menu-icon');
-let closeMenu = document.getElementById('menu-close');
-let nav = document.getElementById('nav');
-menuIcon.addEventListener ('click',function(){
-    nav.style.display ="flex";
-    closeMenu.style.display = "block";
-    nav.parentElement.style.backgroundColor =" hsla(0, 0%, 8%,0.6)";
-});
-closeMenu.addEventListener('click',function(){
-    nav.style.display ="none";
-    closeMenu.style.display = "none";
-    nav.parentElement.style.backgroundColor ="hsl(0, 0%, 98%)";
+
+let mobileMenu = document.querySelectorAll('.mobile-menu');
+
+mobileMenu.forEach(menu => {
+    menu.addEventListener('click',function(event){
+        event.currentTarget.parentElement.classList.toggle("active-m");
+    })
 })
